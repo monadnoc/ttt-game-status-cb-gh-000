9c80bab76b47079f6834ceae503dbe1039f2c375
Jordan Nelson <jordannelson1.2014@u.northwestern.edu>
2019-07-22 18:58:05 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -18,11 +18,14 @@ WIN_COMBINATIONS = [
 def won?(board)
   WIN_COMBINATIONS.each do |combo|
     # win_combination is a 3 element array of indexes that compose a win, [0,1,2]
-    # grab each index from the win_combination that composes a win.
+    # grab each index from the win_combination that composes a win
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
 
-    position_1 = board[combo[0]] # load the value of the board at win_index_1
-    position_2 = board[combo[1]] # load the value of the board at win_index_2
-    position_3 = board[combo[2]] # load the value of the board at win_index_3
+    position_1 = board[win_index_1] # load the value of the board at win_index_1
+    position_2 = board[win_index_2] # load the value of the board at win_index_2
+    position_3 = board[win_index_3] # load the value of the board at win_index_3
     puts combo
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
       return [combo[0], combo[1], combo[2]] # return the win_combination indexes that won.
