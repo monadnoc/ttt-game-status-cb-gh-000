7e1e81746eecc42854ae365ab0aa6a89a316a3f1
Jordan Nelson <jordannelson1.2014@u.northwestern.edu>
2019-07-22 18:26:27 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,7 +15,7 @@ WIN_COMBINATIONS = [
   [1,4,7]
 ]
 
-def won(board)
+def won?(board)
   WIN_COMBINATIONS.each do |combo|
     # win_combination is a 3 element array of indexes that compose a win, [0,1,2]
     # grab each index from the win_combination that composes a win.
