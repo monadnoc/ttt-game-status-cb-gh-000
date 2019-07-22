49b274ae270cd15b4188b3660b2a4ca3216beedd
Jordan Nelson <jordannelson1.2014@u.northwestern.edu>
2019-07-22 19:05:30 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -26,13 +26,13 @@ def won?(board)
     position_1 = board[win_index_1] # load the value of the board at win_index_1
     position_2 = board[win_index_2] # load the value of the board at win_index_2
     position_3 = board[win_index_3] # load the value of the board at win_index_3
-    puts win_index_1
+
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
       return [combo[0], combo[1], combo[2]] # return the win_combination indexes that won.
     elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
       return [combo[0], combo[1], combo[2]]
     else
-      return false
+      false
     end
   end
 end
